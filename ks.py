f0027bf0d4697ca18ce0d32d6eb1bb44123d70ec
Eric Downes <eric@triskew.com>
2022-07-09 19:06:07 -0500

    need to remove time-dep background

--- ks.py
+++ ks.py
@@ -1,46 +1,28 @@
-from typing import *
-from pde import PDEBase, ScalarField, CartesianGrid, MemoryStorage, plot_kymograph
+from numpy import pi
+from pde import PDE, ScalarField, CartesianGrid, MemoryStorage, plot_kymograph
 
-
-NX = 32 # number of cells in x
-TMAX = 100 # time
+NX = 100 # number of cells in x
+TMAX = 220 # time
 DT = .01 # step
-EPSILON = 1/2 # 1/2
 
-
-class KS_PDE(PDEBase):
-    """Implementation of the Kuramoto–Sivashinsky equation
-    https://py-pde.readthedocs.io/en/latest/examples_gallery/pde_custom_class.html
-    """
-    def evolution_rate(self, u:ScalarField,
-                       t:float = 0, eps:float = EPSILON) -> ScalarField:
-        """implement the python version of the evolution equation"""
-        u_xx = u.laplace(bc="auto_periodic_neumann")
-        u_xxxx = u_xx.laplace(bc="auto_periodic_neumann")
-        u_x_sq = u.gradient(bc="auto_periodic_neumann").to_scalar("squared_sum")
-        return -eps * u_x_sq - u_xx - u_xxxx
-
-    def simulate_1d(self,
-                    nx:int = NX,
-                    tmax:float = TMAX,
-                    dt:float = DT,
-                    state:ScalarField = None,
-                    ) -> Union[ScalarField, MemoryStorage]:
-        if not (isinstance(state, ScalarField) and state):
-            grid = CartesianGrid([[0, NX]], [NX], periodic=True)
-            state = ScalarField.random_uniform(grid)
-        else:
-            grid = state.grid
-        self.last_sim = MemoryStorage()
-        eq = PDE({"u": "-gradient_squared(u) / 2 - laplace(u + laplace(u))"})
-        _evolve()
-        final_state = self.solve(state, t_range = tmax,
-                                 tracker = self.last_sim.tracker(dt))
-        return final_state
+def simulate_ks_1d(nx:int = NX,
+                   tmax:float = TMAX,
+                   dt:float = DT,
+                   state:ScalarField = None,
+                   ) -> MemoryStorage:
+    if not (isinstance(state, ScalarField) and state):
+        grid = CartesianGrid([[1, NX]], [NX], periodic=True)
+        state = ScalarField.from_expression(grid, "sin(x)")
+        #state = ScalarField.random_uniform(grid)
+    else:
+        grid = state.grid
+    sim = MemoryStorage()
+    eq = PDE({"u": "-gradient_squared(u) / 2 - laplace(u + laplace(u))"})
+    eq.solve(state, t_range = tmax, dt = dt, tracker = sim.tracker(10 * dt))
+    return sim
 
 
 if __name__ == '__main__':
-    ks = KS_PDE()
-    ks.simulate_1d()
-    plot_kymograph(ks.last_sim)
+    sim = simulate_ks_1d()
+    plot_kymograph(sim)
 
